042f2619786908ab27a4cceaafe4f643fc45a5b2
Werefox <jack@wrightus.com>
2022-06-27 19:30:03 -0400

    Stubbed grocerySlice

---
+++ src/features/grocerySlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+
+}
+
+export const grocerySlice = createSlice({
+	name: 'grocery',
+	initialState,
+	reducers: {
+		
+	}
+})
+
+// export const reducers
+
+export default grocerySlice.reducer
