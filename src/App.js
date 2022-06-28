929ec39f73c64ac3c6cc8b3edc99a2fbcc21ae12
Werefox <jack@wrightus.com>
2022-06-27 20:51:06 -0400

    Added nav

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
+import { useState } from 'react';
 import Form from './components/Form';
 import List from './components/List';
+import Navbar from './components/Navbar';
 import Table from './components/Table';
 
 function App() {
+	let [view, setView] = useState(false)
+
 	return (
 		<div className="App">
-			<Form />
-			<List />
+			<div>
+				Title <button onClick={() => setView(true)}>View as List</button> <button onClick={() => setView(false)}>View as Table</button>
+			</div>
 			<hr />
-			<Table />
+			<Form />
+			{view ? <List /> : <Table />}
 		</div>
 	);
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+	return (
+		<div>
+			<h1>Title</h1> <button >View as List</button> <button>View as Table</button>
+		</div>
+	)
+}
+
+export default Navbar
