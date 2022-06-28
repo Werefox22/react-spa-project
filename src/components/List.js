459703c7556663830d90f6231792a87f62d6e875
Werefox <jack@wrightus.com>
2022-06-27 20:06:17 -0400

    displayed the list data in the list component

--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { useSelector, useDispatch } from 'react-redux'
+
 
 function List() {
+	const list = useSelector((state) => state.inventory.list)
+
+	const mappedItems = list.map((item, index) => {
+		return (
+			<li key={index}>
+				{item.name} ({item.quantity})
+			</li>
+		)
+	})
+
 	return (
 		<ul>
-			<li>
-				List Item
-			</li>
+			{mappedItems}
 		</ul>
 	)
 }
